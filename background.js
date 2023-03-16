6a41f8ed81efc639547f9aa34cbc2be2df9e5d19
Terrence Scoville <terrence.scoville@goldenhippo.com>
2023-03-15 19:40:09 -0700

    Dark mode native is here, UI cleanup

--- background.js
+++ background.js
@@ -3,6 +3,18 @@ function scrambleList() {
   for (var i = ul.children.length; i >= 0; i--) {
       ul.appendChild(ul.children[Math.random() * i | 0]);
   }
+
+  var nav = document.querySelector('#ak-jira-navigation');
+  nav.style.display = 'none';
+
+  var header = document.querySelector('#ghx-header');
+  header.style.display = 'none';
+
+  var breadcrumbs = document.querySelector('.bYQimP');
+  breadcrumbs.style.display = 'none';
+
+  window.dispatchEvent(new Event('resize'));
+  
   console.log('SCRAMBLED');
 }
 
