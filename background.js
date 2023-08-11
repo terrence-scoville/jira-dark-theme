57db445c739e5a7092c7a9705efec7a8d37aa9fc
Terrence Scoville <terrence.scoville@goldenhippo.com>
2023-08-11 09:44:03 -0700

    Updates for enhanced boards

--- background.js
+++ background.js
@@ -1,17 +1,42 @@
 function scrambleList() {
-  var ul = document.querySelector('#ghx-quick-filters > ul:last-child');
+  // var ul = document.querySelector('#ghx-quick-filters > ul:last-child');
+  // for (var i = ul.children.length; i >= 0; i--) {
+  //     ul.appendChild(ul.children[Math.random() * i | 0]);
+  // }
+
+  var ul = document.querySelector('._otyrzwfg > fieldset');
   for (var i = ul.children.length; i >= 0; i--) {
       ul.appendChild(ul.children[Math.random() * i | 0]);
   }
 
+  var sidebar = document.querySelector('#ak-side-navigation');
+  sidebar.style.display = 'none';
+
   var nav = document.querySelector('#ak-jira-navigation');
   nav.style.display = 'none';
 
-  var header = document.querySelector('#ghx-header');
-  header.style.display = 'none';
+  // var header = document.querySelector('#ghx-header');
+  // header.style.display = 'none';
 
-  var breadcrumbs = document.querySelector('.bYQimP');
-  breadcrumbs.style.display = 'none';
+  setTimeout(() => {
+
+    var header = document.querySelector('.css-1q41chj');
+    header.style.gridTemplateRows = '0px 0px auto';
+
+    var main = document.querySelector('#ak-main-content');
+    main.style.height = '100vh';
+
+    var mainChild = document.querySelector('#ak-main-content > div:first-child');
+    mainChild.style.height = '100vh';
+
+    var breadcrumbs = document.querySelector('._1tke1tcg');
+    breadcrumbs.style.display = 'none';
+
+    var title = document.querySelector('.css-1pa8dxh');
+    title.style.display = 'none';
+
+  }, "1000");
+
 
   window.dispatchEvent(new Event('resize'));
   
